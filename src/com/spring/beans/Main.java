5b49358701eafa052c49074250764b02f7ee43a4
pan175 <1751265292@qq.com>
2018-08-02 09:42:12 +0800

    通过静态工厂方法和实例工厂方法创建Bean

--- src/com/spring/beans/Main.java
+++ src/com/spring/beans/Main.java
@@ -1,5 +1,7 @@
 package com.spring.beans;
 
+import java.util.Date;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,15 +9,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Person person1 = (Person) ac.getBean("person1");
-		System.out.println(person1);
+//		Person person1 = (Person) ac.getBean("person1");
+//		System.out.println(person1);
 		
 //		Person person2 = (Person) ac.getBean("person2");
 //		System.out.println(person2);
 		
-		Car car1 = (Car) ac.getBean("car1");
+//		Car car1 = (Car) ac.getBean("car1");
 //		Car car2 = (Car) ac.getBean("car1");
-		System.out.println(car1);//值为true
+//		System.out.println(car1);//值为true
 		
+		/*DateFormat df = (DateFormat) ac.getBean("dateFormat");
+		System.out.println(df);*/
+		
+		Date d = (Date) ac.getBean("date");
+		System.out.println(d);
 	}
 }
